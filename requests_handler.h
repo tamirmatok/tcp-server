fcbce0d27e5718720522133cd9edb727607c6941
tamirmatok <95190977+tamirmatok@users.noreply.github.com>
2023-01-10 19:45:47 +0200

    Initial project files

---
+++ requests_handler.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "server.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <direct.h>
+#define GetCurrentDir _getcwd
+
+
+extern const string RESPONSE_OK;
+extern const string RESPONSE_CREATED_FILE;
+extern const string RESPONSE_NO_CONTENT;
+extern const string RESPONSE_FILE_NOT_FOUND;
+extern const string SERVER_ERROR;
+
+string requestHandler(Recv_headers& msg_headers, char* sendBuff);
+string get_full_path(string fileName);
+string handleGetRequest(Recv_headers& msg_headers);
+string getFileName(string language);
+string createResponse(string status, string contentType, string contentLen, string body, bool isOptions);
